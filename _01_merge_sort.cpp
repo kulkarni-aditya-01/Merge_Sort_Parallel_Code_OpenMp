fa906f2874fbed96dc261cbd072d0cfe71519a4c
kulkarni-aditya-01 <coder.programmer108@gmail.com>
2026-04-24 02:39:27 +0530

    Initial commit with C++ main file of the algo:)

---
+++ _01_merge_sort.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+#define MAX_INPUT 1000001
+
+
+void merge(int *arr, int left, int mid, int right){
+	
+	int *temp = (int *)malloc(sizeof(int)*(right-left+1));
+	int k=0 ;	
+	int i=left; 
+	int j=mid+1; 	
+	while(i<=mid && j<=right){
+		if(arr[i] < arr[j]){
+			temp[k] = arr[i]; 
+			i++ ;
+		}
+		else{
+			temp[k] = arr[j] ;
+			j++; 
+		}
+		k++ ;
+	}
+
+	while(i<=mid){
+		temp[k] = arr[i] ;
+		i++; 
+		k++; 
+	}
+
+	while(j<=right){
+		temp[k] = arr[j]; 
+		j++; 
+		k++ ;
+	}
+
+	//now add to the same loop ;
+	for(int i=0; i<k ; i++){
+		arr[left+i] = temp[i] ;
+	}
+	free(temp);
+	return ;
+}
+
+
+void merge_sort(int *arr, int left, int right){
+	
+	if(left>=right){
+		return ; 
+	}
+
+	int mid = (left+right)/2; 
+	merge_sort(arr, left, mid);
+	merge_sort(arr, mid+1, right);
+	merge(arr, left, mid, right); 
+}
+
+
+int main(int argc, char *argv[]){
+//	int n;
+//	std:: cout << "Enter the total number of the elements : " << std::endl; 
+//	std:: cin >> n ;
+
+	int *arr = (int *)malloc(MAX_INPUT*sizeof(int));
+
+	FILE *read_int ;
+	read_int = fopen(argv[1],"r") ;
+
+	int count=0 ;
+	char buff[50] ;
+
+	while(fgets(buff, sizeof(buff), read_int)){
+		int num;
+		sscanf(buff, "%d", &num);
+		arr[count++] = num ;
+		if(count >= MAX_INPUT-1) break ;
+	}
+	fclose(read_int);
+	
+	merge_sort(arr, 0, count-1);
+	
+	for(int i=0;i<count;i++){
+		std::cout<<arr[i]<<" " ; 
+	}
+	std::cout<<"\n" ;
+	
+	free(arr);
+
+	return 0 ;
+}
+
